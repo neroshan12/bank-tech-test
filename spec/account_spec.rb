7600d7a35168a1318424183ff475d02fa689a436
Nero Siva <neroshan12@gmail.com>
2017-10-30 14:55:29 +0000

    Refactor tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,24 +1,21 @@
 require 'account'
 
 describe Account do
+  subject(:account) {described_class.new}
   it 'initializes a new account' do
-    account = Account.new
     expect(account).to be_instance_of(Account)
   end
 
   it 'initializes with a balance of 0' do
-    account = Account.new
     expect(account.balance).to eq(0)
   end
 
   it 'allows the debiting of an account' do
-    account = Account.new
     account.debit(10)
     expect(account.balance).to eq(10)
   end
 
   it 'allows the crediting of an account' do
-    account = Account.new
     account.debit(10)
     account.credit(5)
     expect(account.balance).to eq(5)
